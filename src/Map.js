5d1d2a4b08aa32f7110e9c9930c480012c290df2
Anurag Singhal <anusing@adobe.com>
2020-03-20 04:03:27 +0530

    Working code

--- src/Map.js
+++ src/Map.js
@@ -1,157 +1,124 @@
 import React from 'react';
-import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import Tabletop from 'tabletop';
+import './Map.css';
 
-import './App.css';
 const HighMaps = require('react-highcharts/ReactHighmaps');
-const Highcharts = require('highcharts')
-const indidaMapData = require('./mapdata/india');
-
-const useStyles = makeStyles(theme => ({
-    typography: {
-        padding: theme.spacing(10),
-    },
-}));
-const classes = useStyles();
-
-
-
-
-const indiaCurrentData = [
-    ['in-an', 0],
-    ['in-wb', 1],
-    ['in-ld', 2],
-    ['in-5390', 3],
-    ['in-py', 4],
-    ['in-3464', 5],
-    ['in-mz', 6],
-    ['in-as', 7],
-    ['in-pb', 8],
-    ['in-ga', 9],
-    ['in-2984', 10],
-    ['in-jk', 11],
-    ['in-hr', 12],
-    ['in-nl', 13],
-    ['in-mn', 14],
-    ['in-tr', 15],
-    ['in-mp', 16],
-    ['in-ct', 17],
-    ['in-ar', 18],
-    ['in-ml', 19],
-    ['in-kl', 20],
-    ['in-tn', 21],
-    ['in-ap', 22],
-    ['in-ka', 23],
-    ['in-mh', 24],
-    ['in-or', 25],
-    ['in-dn', 26],
-    ['in-dl', 27],
-    ['in-hp', 28],
-    ['in-rj', 29],
-    ['in-up', 30],
-    ['in-ut', 31],
-    ['in-jh', 32],
-    ['in-ch', 33],
-    ['in-br', 34],
-    ['in-sk', 35]
-];
-
-class Info extends React.Component {
-
-    render() {
-        return <div>No cases present in this region</div>;
-    }
-}
+const indiaMapData = require('./mapdata/india-all-distributed');
 
 class Maps extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showDialog: false,
-            stateName: ''
+            keyName: null,
+            dataAvailable: false
         }
-        this.handleClose = this.handleClose.bind(this);
-        this.config = {
-            tooltip: {
-                distance: 2,
-                followPointer: false, 
-                followTouchMove: false,
-                backgroundColor: "#fff",
-                borderWidth: 0,
-                shadow: false
-            },
-            colorAxis: {
-                stops: [
-                    [0, '#e8f7e9'],
-                    [0.5, '#ea772a'],
-                    [1, '#fc1307']
-                ],
-                min: 0
-            },
-            legend: {
-                title: {
-                    text: "Confirmed COVID Cases"
-                },
-                align: "left",
-                verticalAlign: "bottom",
-                valueDecimals: 0,
-                backgroundColor: "rgba(255,255,255,0.9)",
-                symbolRadius: 0,
-                symbolHeight: 14
-            },
-            series: [{
-                data: indiaCurrentData.map(val => {return {key: val[0], value: val[1]}}),
-                mapData: indidaMapData,
-                joinBy: ['hc-key', 'key'],
-                name: 'Covid',
-                states: {
-                    hover: {
-                        color: Highcharts.getOptions().colors[3]
-                    }
-                },
-                point: {
-                    events: {
-                        click: function(evt) {
-                            var key = this.key;
-                            this.events.showDialogBox(key)
-                        },
-                        showDialogBox: (key) => {
-                            this.setState({
-                                stateName: key,
-                                showDialog: true
-                            });
-                        }
-                    }
-                }
-            }]
-        }
+        this.renderDetails = this.renderDetails.bind(this);
     }
 
-    handleClose() {
-        this.setState({ showDialog: false });
+
+    processData(googleData) {
+        let data = [...googleData];
+
+        data = data.map(row => {
+            return {
+                code: row.code.toLowerCase(),
+                name: row.name,
+                value: parseFloat(row.total),
+                deaths: row.deaths,
+                recoveries: row.recoveries
+            }
+        });
+        return data;
+    }
+
+    componentDidMount() {
+        Tabletop.init({
+            key: '1dK3kzGfe5Kpn5-DQHQvJ6-qzq_4lNaX8fkfztPhlzfQ',
+            callback: googleData => {
+                // console.log('google sheet data --->', googleData)
+                const newData = this.processData(googleData)
+                this.config = {
+                    chart: {
+                        height: 800
+                    },
+                    title: {
+                        text: "COVID India Cases"
+                    },
+                    tooltip: {
+                        backgroundColor: "#fff",
+                        borderWidth: 0,
+                        distance: 2,
+                        followPointer: false,
+                        followTouchMove: false,
+                        shadow: false,
+                        useHTML: true,
+                        pointFormat:
+                            '<span class="f32"><span class="flag"></span></span>' +
+                            "<span class='f44'> {point.code}: <b>{point.value}</b> cases </span> <br /> <span class='f44'>Deaths: {point.deaths}</span> <br /> <span class='f44'>Recoveries: {point.recoveries}</span>",
+                    },
+                    colorAxis: {
+                        stops: [
+                            [0, '#e8f7e9'],
+                            [0.5, '#ea772a'],
+                            [1, '#fc1307']
+                        ],
+                        min: 0
+                    },
+                    legend: {
+                        title: {
+                            text: "Confirmed COVID Cases"
+                        },
+                        align: "left",
+                        verticalAlign: "bottom",
+                        valueDecimals: 0,
+                        backgroundColor: "rgba(255,255,255,0.9)",
+                        symbolRadius: 0,
+                        symbolHeight: 14
+                    },
+                    series: [{
+                        data: newData,
+                        mapData: indiaMapData,
+                        joinBy: null,
+                        name: 'Covid',
+                        states: {
+                            hover: {
+                                color: "#07cffc"
+                            }
+                        },
+                        point: {
+                            events: {
+                                click: function (evt) {
+                                    var code = this.code;
+                                    this.events.showPopOver(code)
+                                },
+                                showPopOver: (key) => {
+                                    this.setState({
+                                        open: true,
+                                        keyName: key
+                                    });
+                                }
+                            }
+                        },
+                        dataLabels: {
+                            enabled: true,
+                            format: "{point.job}"
+                        }
+                    }]
+                }
+                this.setState({ dataAvailable: true });
+            },
+            simpleSheet: true
+        });
+    }
+
+    renderDetails() {
+        return <div className="detailed-dialog"> This contains name {this.state.keyName}</div>
     }
 
     render() {
-        return <div>
-            <HighMaps config={this.config} />
-            <div className="dummyClick" ref={ref => {
-                this.dummyDiv = ref;}}></div>
-            <Popover
-                open={this.state.showDialog}
-                anchorEl={this.dummyDiv}
-                onClose={this.handleClose}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                }}
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                }}
-            >
-                <Typography className={classes.typography}>The content of the Popover.</Typography>
-            </Popover>
+        return <div className="india-map-detailed-view">
+            {this.state.dataAvailable && <div className="india-map"><HighMaps config={this.config}/></div>}
+            {this.state.keyName && this.renderDetails()}
         </div>
     }
 }
